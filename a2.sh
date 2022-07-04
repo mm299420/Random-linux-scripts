7908667a3c5f76ff588cd73e517e3a96b88d73e8
MM29942 <52143479+mm299420@users.noreply.github.com>
2022-07-04 12:14:15 +0200

    Create a2.sh

---
+++ a2.sh
@@ -0,0 +1,9 @@
+su mm29942
+mkdir ~/Aufgaben/DateibaumOps/Aufrg2.1/DortObenAufDenBergen
+cat  "Da oben, auf'm Berge, da steht eine Kuh, die macht ihr A…loch auf und zu." >> vers1.txt
+cat "||:Holla dri hi ja, holla drio, holla dri hi ja, holla drio:||" >> refrain.txt
+cat "Und unten im Tale, da stehet ein Ochs, der hat dieser Kuh ins A....loch geglotzt." >> vers2.txt
+cat "Ich steh' auf 'ner Brücke und spucke in'n Kahn, da freut sich die Spuck‘, dass sie Kahn fahren kann." >> vers3.txt
+mkdir ../Aufgaben2.2
+cp -r . ../Aufgaben2.2/DortObenAufDenBergen2
+cat "Da oben, auf'm Berge, da klappert der Storch, und unten im Tale, da ferkelt der Borch." >> versX.txt
